87e1f2efe7b8c0f1ae4188262ab6ed9f87079d8f
oOxygen73 <45863533+oOxygen73@users.noreply.github.com>
2018-12-14 03:01:28 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,12 +1,10 @@
 <?php
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
-
 if(!$update)
 {
   exit;
 }
-
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
@@ -15,11 +13,26 @@ $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name']
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
-
 $text = trim($text);
 $text = strtolower($text);
-
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
+$response = '';
+if(strpos($text, "/start") === 0 || $text=="ciao")
+{
+	$response = "Ciao $firstname, benvenuto!";
+}
+elseif($text=="ciao")
+{
+	$response = "Ciao, benvenuto $firstname";
+}
+elseif($text=="oxy")
+{
+	$response = "oOxygen è il mio Creatore";
+}
+else
+{
+	$response = "Comando non valido!";
+}
+$parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
