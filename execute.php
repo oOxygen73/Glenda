8fd45a4a456e8113ad43c40b1a25e53e74b2ad69
oOxygen73 <45863533+oOxygen73@users.noreply.github.com>
2020-02-11 02:57:38 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -26,63 +26,45 @@ if (strpos($a, 'oxy') !== false) {
 	switch ($Message)
 	{
 		case '/start':
-			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Glenda.jpg"; 
-			$photoDesc ="Ciao $FirstName io Sono Glenda. \nCome Posso Esserti Utile? \nPuoi Usare il Menu qui Sotto.";
-			sendStartImage($ChatID, $photoUrl, $photoDesc);
+			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Exorcist.jpg"; 
+			$photoDesc ="<b>Ciao io Sono Regan MacNeil. Non Posso Esserti Utile.</b>";
+			sendMessageImageAndLinks($ChatID, $photoUrl, $photoDesc);
 			break;
 			
 		case 'Menu':
-			$msg = "<b>Menu Attivato!</b>";
-			showKeyboard($ChatID, $msg);
+			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Exorcist.jpg"; 
+			$photoDesc ="<b>Ciao io Sono Regan MacNeil. Non Posso Funzionare.</b>";
+			sendMessageImageAndLinks($ChatID, $photoUrl, $photoDesc);
 			break;
 			
 		case 'Nothing':
-			$message_body = "<b>oOxygen non c'è! Non taggatelo, Grazie.</b>";
-			sendMessageParseHtml($ChatID, $message_body);
+			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Exorcist.jpg"; 
+			$photoDesc ="<b>oOxygen non c'è! Non taggatelo, Grazie.</b>";
+			sendMessageImageAndLinks($ChatID, $message_body);
 			break;
 			
 		case 'NothingHere':
-			$message_body = "<b>oOxy non c'è mi dispiace, non so piu come dirvelo</b>";
-			sendMessageParseHtml($ChatID, $message_body);
+			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Exorcist.jpg"; 
+			$photoDesc ="<b>oOxy non c'è mi dispiace, non so piu come dirvelo</b>";
+			sendMessageImageAndLinks($ChatID, $message_body);
 			break;
 			
 		case 'Glenda':
-			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Glenda.jpg"; 
-			$photoDesc ="<b>Ciao $FirstName io Sono Glenda. \nCome Posso Esserti Utile? \nPuoi Aprire una Chat Privata con me se Vuoi.</b>";
+			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Exorcist.jpg"; 
+			$photoDesc ="<b>Io Sono Regan MacNeil. \nNon Posso Esserti Utile e non Evocarmi.</b>";
 			sendMessageImageAndLinks($ChatID, $photoUrl, $photoDesc);
 			break;
 			
 		case 'Links':
-			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Logo2.png"; 
-			$photoDesc ="<b>Eccoti i Nostri Links</b> \n<a href='http://insane3dPrinting-forum.ooxygen.tech/'>Insane3dPrinting Forum</a> \n<a href='http://insane3d.ooxygen.tech/'>Insane3dPrinting Downloads</a>";
+			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Exorcist.jpg"; 
+			$photoDesc ="<b>Non ho Links da Darti al Momento.</b>";
 			sendMessageImageAndLinks($ChatID, $photoUrl, $photoDesc);
 			break;
 
 		case 'Tools': // Command to show normal Keyboard
-			$photoUrl ="http://ooxygen.tech/Glenda_Bot/InsaneStepCalculator2.jpg"; 
-			$photoDesc ="<b>Scarica il Nostro Programma e i Nostri Tools</b> \n<a href='http://insane3d.ooxygen.tech/AppTools/Insane_Step_Calculator.rar'>Insane_Step_Calculator v1.0.x</a> \n<a href='http://insane3d.ooxygen.tech/AppTools/Cubo_di_Calibrazione_30x30.rar'>Cubo_di_Calibrazione_30x30</a> \n<a href='http://insane3d.ooxygen.tech/AppTools/Cubo_di_Calibrazione_40x40.rar'>Cubo_di_Calibrazione_40x40</a>\n<a href='http://insane3d.ooxygen.tech/AppTools/Temp_Tower_Pla.rar'>Temp_Tower_Pla</a>\n<a href='http://insane3d.ooxygen.tech/AppTools/Test_Retraction_a_2_Torri.rar'>Test_Retraction_a_2_Torri</a>\n<a href='http://insane3d.ooxygen.tech/AppTools/Test_Retraction_a_4_Torri.rar'>Test_Retraction_a_4_Torri</a>\n<a href='http://insane3d.ooxygen.tech/AppTools/Portachiavi_insane3d.rar'>Portachiavi_insane3d</a>";
+			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Exorcist.jpg"; 
+			$photoDesc ="<b>Non ho Tools per te al Momento.</b>";
 			sendMessageImageAndLinks($ChatID, $photoUrl, $photoDesc);
-			break;
-
-		case "Infos":
-			$message_body = "<b>Sono Glenda un Utility Bot Creato per Questo Gruppo. \nApri una Chat con Me in Privato.</b>";
-			sendMessageParseHtml($ChatID, $message_body);
-			break;
-
-		case "Help": // This is the same text inside a Keyboard
-			$message_body = "<b>Se ti Serve Aiuto puoi Consultare il Nostro Sito. \nOppure Chiedere ai Membri del Nostro Staff.</b>";
-			sendMessageParseHtml($ChatID, $message_body);
-			break;
-			
-		case "test": // This is the same text inside a Keyboard
-			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Logo.png"; 
-			$photoDesc ="<b>Eccoti i Nostri Links</b> \n<a href='http://insane3dPrinting-forum.ooxygen.tech/'>Insane3dPrinting Forum</a> \n<a href='http://insane3d.ooxygen.tech/'>Insane3dPrinting Downloads</a>";
-			sendMessageImageAndLinks($ChatID, $photoUrl, $photoDesc);
-			break;
-
-		case 'Buongiorno':
-			$message_body = "<b>Buongiorno io Sono Glenda. \nPuoi Attivarmi Digitando il mio Nome.</b>";
-			sendMessageParseHtml($ChatID, $message_body);
 			break;
 
 		case "Inline Keyboard": // This is the same text inside a Keyboard
