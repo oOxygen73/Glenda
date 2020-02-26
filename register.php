7c12d0f8c796ff15bb701ad69924339a4cef646c
oOxygen73 <45863533+oOxygen73@users.noreply.github.com>
2020-02-27 00:59:54 +0100

    Create register.php

---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+
+// PARAMETRI DA MODIFICARE
+$WEBHOOK_URL = 'https://glendabot.herokuapp.com/glenda.php';
+$BOT_TOKEN = '959465967:AAGL96nATh_YOb6E6mVXigx24PMr4iNan04';
+
+// NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
+$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
+$method = 'setWebhook';
+$parameters = array('url' => $WEBHOOK_URL);
+$url = $API_URL . $method. '?' . http_build_query($parameters);
+$handle = curl_init($url);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+$result = curl_exec($handle);
+print_r($result);
