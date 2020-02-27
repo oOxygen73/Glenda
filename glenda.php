c5f1bdb378ac649cd4f52bc04d76c6e714f9b8ce
oOxygen73 <45863533+oOxygen73@users.noreply.github.com>
2020-02-27 02:56:26 +0100

    Update glenda.php

--- glenda.php
+++ glenda.php
@@ -1,47 +1,29 @@
 <?php
-	
-	$botToken = "959465967:AAGL96nATh_YOb6E6mVXigx24PMr4iNan04"; // Api TOKEN to our bot
-	$website = "https://api.telegram.org/bot".$botToken;
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 
-if(!$update)
-{
-  exit;
+if (! $update) {
+    exit;
 }
 
-$message = isset($update['message']) ? $update['message'] : "";
-$messageId = isset($message['message_id']) ? $message['message_id'] : "";
-$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
-$date = isset($message['date']) ? $message['date'] : "";
-$text = isset($message['text']) ? $message['text'] : "";
+$chatId = $update['message']['chat']['id'] ?? null;
 
-$text = trim($text);
-$text = strtolower($text);
-header("Content-Type: application/json");
-
-$response = '';
-
-if(strpos($text, "/start") === 0 || $text=="ciao")
-{
-	$response = "Ciao $firstname, benvenuto!";
-}
-elseif($text=="domanda 1")
-{
-	$response = "risposta 1";
-}
-elseif($text=="domanda 2")
-{
-	$response = "risposta 2";
-}
-else
-{
-	$response = "Comando non valido!";
+if ($channelName = $update['channel_post']['chat']['username'] ?? null) {
+    $chatId = sprintf('@%s', $channelName);
 }
 
-$parameters = array('chat_id' => $chatId, "text" => $response);
-$parameters["method"] = "sendMessage";
-echo json_encode($parameters);
+if (! $chatId) {
+    exit;
+}
+
+header('Content-Type: application/json');
+$dump = json_encode($update, JSON_PRETTY_PRINT);
+
+echo json_encode(
+    [
+        'chat_id' => $chatId, 
+        'text' => "```\n$dump\n```", 
+        'method' => 'sendMessage', 
+        'parse_mode' => 'markdown'
+    ]
+);
