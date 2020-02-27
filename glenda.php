1dafc6480a26d81f9f1fdbe30c8dbec2df51ac30
oOxygen73 <45863533+oOxygen73@users.noreply.github.com>
2020-02-27 03:38:25 +0100

    Update glenda.php

--- glenda.php
+++ glenda.php
@@ -1,32 +1,36 @@
 <?php
-header('Content-Type: application/json');
-$content = file_get_contents('php://input');
+// recupero il contenuto inviato da Telegram
+$content = file_get_contents("php://input");
+// converto il contenuto da JSON ad array PHP
 $update = json_decode($content, true);
-
-if (!$update) {
-    exit;
+// se la richiesta è null interrompo lo script
+if(!$update)
+{
+  exit;
 }
-
-$chatId = $update['message']['chat']['id'] ?? null;
-$text = $update['message']['text'] ?? 'Inserire un testo valido';
-$FirstName = $update['chat']['first_name'] ?? null;
-$parameters = [];
-
-$parameters['chat_id'] = $chatId;
-$parameters['text'] = $text;
-$parameters['first_name'] = $FirstName;
-$parameters['method'] = 'sendMessage';
-
-$parameters['reply_markup'] = [
-    'keyboard' => [
-        ['Spaghetti Cacio e pepe', 'Spaghetti alla Carbonara'],
-        ['Pappardelle al Ragù', 'Pappardelle ai Funghi'],
-        ['Strozzapreti al Pesto', 'Fusilli al pomodoro'],
-        ['Test 1', 'Test 2', 'Test 3'],
-        ['Nuova comanda [010]', 'Precedente comanda [009]']
-    ],
-    'one_time_keyboard' => true,
-    'resize_keyboard' => true
-];
-
+// assegno alle seguenti variabili il contenuto ricevuto da Telegram
+$message = isset($update['message']) ? $update['message'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
+$date = isset($message['date']) ? $message['date'] : "";
+$text = isset($message['text']) ? $message['text'] : "";
+// pulisco il messaggio ricevuto togliendo eventuali spazi prima e dopo il testo
+$text = trim($text);
+// converto tutti i caratteri alfanumerici del messaggio in minuscolo
+$text = strtolower($text);
+// mi preparo a restitutire al chiamante la mia risposta che è un oggetto JSON
+// imposto l'header della risposta
+header("Content-Type: application/json");
+// la mia risposta è un array JSON composto da chat_id, text, method
+// chat_id mi consente di rispondere allo specifico utente che ha scritto al bot
+// text è il testo della risposta
+$parameters = array('chat_id' => $chatId, "text" => $text);
+// method è il metodo per l'invio di un messaggio (cfr. API di Telegram)
+$parameters["method"] = "sendMessage";
+// imposto la keyboard
+$parameters["reply_markup"] = '{ "keyboard": [["uno"], ["due"], ["tre"], ["quattro"]], "one_time_keyboard": false}';
+// converto e stampo l'array JSON sulla response
 echo json_encode($parameters);
