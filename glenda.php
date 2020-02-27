4de663f54b38055a3642b50cebf18b0428be4789
oOxygen73 <45863533+oOxygen73@users.noreply.github.com>
2020-02-27 02:19:22 +0100

    Update glenda.php

--- glenda.php
+++ glenda.php
@@ -23,6 +23,6 @@ $text = trim($text);
 $text = strtolower($text);
 
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
+$parameters = array('chat_id' => $chatId, "text" => $text, "firstname" => $firstname);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
