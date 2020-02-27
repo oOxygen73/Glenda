a5d077986e8d868f82915e951a677cff8077397c
oOxygen73 <45863533+oOxygen73@users.noreply.github.com>
2020-02-27 04:24:06 +0100

    Update glenda.php

--- glenda.php
+++ glenda.php
@@ -1,154 +1,59 @@
 <?php
-	
+
 	$botToken = "959465967:AAGL96nATh_YOb6E6mVXigx24PMr4iNan04"; // Api TOKEN to our bot
 	$website = "https://api.telegram.org/bot".$botToken;
+    $FilejSon = file_get_contents(php://input);
+    $FilejSon = json_decode($FilejSon, TRUE);
+    $FirstName = $FilejSon[message][chat][first_name];
+    $UserChatId = $FilejSon[message][chat][id];
+    $Message = $FilejSon[message][text];
+    $a = ['Buongiorno', 'Buongiorno Cavaliere!', 'Bentrovato compagno!'];
 
-	$FilejSon = file_get_contents("php://input"); // Take the url input, in this case will be executed method getUpdates that return Update.
-	$FilejSon = json_decode($FilejSon, TRUE); // Decode the variable before because now we can search with key (because it's a dictionary)
+    switch ($Message)
+    {
+        case '/vai':
+            $msg = Ciao $GLOBALS[FirstName]!;
+            showKeyboard($UserChatId, $msg);
+            break;
 
-	$FirstName = $FilejSon["message"]["chat"]["first_name"]; // Get the name that user set
-	$ChatID = $FilejSon["message"]["chat"]["id"]; // get the User ID, this is unique
-	$Message = $FilejSon["message"]["text"]; // Get the message sent from user
-    $messageId = $FilejSon["message"]["message_id"]; // get the User ID, this is unique
-	$message = isset($update['message']) ? $update['message'] : "";
-	header("Content-Type: application/json");
-	$a = $Message;
+        case 'Buongiorno':
+            $msg = $a[mt_rand(0, count($a) - 1)];
+            sendMessage($UserChatId, $msg);
+            break;
 
-if (strpos($a, 'oOxygen_Tech') !== false) {
-    //echo 'true';
-	$Message = 'Nothing';
-}
+        case  Conigli : // This is the same text inside a Keyboard
+            $msg = La difficoltà EX dei conigli è disponibile nei seguenti orari: 00:00/05:00/13:00/16:00;
+            showKeyboard($UserChatId, $msg);
+            break;
 
-if (strpos($a, 'oxy') !== false) {
-    //echo 'true';
-	$Message = 'NothingHere';
-}
+        case Inline Keyboard:
+            $msg = Abracadabra and inline keyboard will appear!;
+            inlineKeyboard($UserChatId, $msg);
+            break;
 
-	switch ($Message)
-	{
-		case '/start':
-			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Glenda.jpg"; 
-			$photoDesc ="Ciao $FirstName io Sono Glenda. \nCome Posso Esserti Utile? \nPuoi Usare il Menu qui Sotto.";
-			sendStartImage($ChatID, $photoUrl, $photoDesc);
-			break;
-			
-		case 'Menu':
-			$msg = "<b>Menu Attivato!</b>";
-			showKeyboard($ChatID, $msg);
-			break;
-			
-		case 'Nothing':
-			$message_body = "<b>oOxygen non c'è! Non taggatelo, Grazie.</b>";
-			sendMessageParseHtml($ChatID, $message_body);
-			break;
-			
-		case 'NothingHere':
-			$message_body = "<b>oOxy non c'è mi dispiace, non so piu come dirvelo</b>";
-			sendMessageParseHtml($ChatID, $message_body);
-			break;
-			
-		case 'Glenda':
-			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Glenda2.0.jpg"; 
-			$photoDesc ="<b>Ciao $FirstName io Sono Glenda. \nCome Posso Esserti Utile? \nPuoi Aprire una Chat Privata con me se Vuoi.</b>";
-			sendMessageImageAndLinks($ChatID, $photoUrl, $photoDesc);
-			break;
-			
-		case 'test':
-			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Glenda2.0.jpg"; 
-			$photoDesc ="<b>Ciao $GLOBALS[FirstName]! io Sono Glenda.</b>";
-			sendMessageImageAndLinks($ChatID, $photoUrl, $photoDesc);
-			break;
-			
-		case 'Links':
-			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Logo2.png"; 
-			$photoDesc ="<b>Eccoti i Nostri Links</b> \n<a href='http://insane3dPrinting-forum.ooxygen.tech/'>Insane3dPrinting Forum</a> \n<a href='http://insane3d.ooxygen.tech/'>Insane3dPrinting Downloads</a>";
-			sendMessageImageAndLinks($ChatID, $photoUrl, $photoDesc);
-			break;
+        case Remove Keyboard:
+            $msg = Abracadabra and keyboard will disappear!;
+            removeKeyboard($UserChatId, $msg);
+            break;
 
-		case 'Tools': // Command to show normal Keyboard
-			$photoUrl ="http://ooxygen.tech/Glenda_Bot/InsaneStepCalculator2.jpg"; 
-			$photoDesc ="<b>Scarica il Nostro Programma e i Nostri Tools</b> \n<a href='http://insane3d.ooxygen.tech/AppTools/Insane_Step_Calculator.rar'>Insane_Step_Calculator v1.0.x</a> \n<a href='http://insane3d.ooxygen.tech/AppTools/Cubo_di_Calibrazione_30x30.rar'>Cubo_di_Calibrazione_30x30</a> \n<a href='http://insane3d.ooxygen.tech/AppTools/Cubo_di_Calibrazione_40x40.rar'>Cubo_di_Calibrazione_40x40</a>\n<a href='http://insane3d.ooxygen.tech/AppTools/Temp_Tower_Pla.rar'>Temp_Tower_Pla</a>\n<a href='http://insane3d.ooxygen.tech/AppTools/Test_Retraction_a_2_Torri.rar'>Test_Retraction_a_2_Torri</a>\n<a href='http://insane3d.ooxygen.tech/AppTools/Test_Retraction_a_4_Torri.rar'>Test_Retraction_a_4_Torri</a>\n<a href='http://insane3d.ooxygen.tech/AppTools/Portachiavi_insane3d.rar'>Portachiavi_insane3d</a>";
-			sendMessageImageAndLinks($ChatID, $photoUrl, $photoDesc);
-			break;
+        default:
+            $msg = Unknown Command! So sorry ;(;
+            sendMessage($ChatId, $msg);
+            break;
+    } 
 
-		case "Infos":
-			$message_body = "<b>Sono Glenda un Utility Bot Creato per Questo Gruppo. \nApri una Chat con Me in Privato.</b>";
-			sendMessageParseHtml($ChatID, $message_body);
-			break;
 
-		case "Help": // This is the same text inside a Keyboard
-			$message_body = "<b>Se ti Serve Aiuto puoi Consultare il Nostro Sito. \nOppure Chiedere ai Membri del Nostro Staff.</b>";
-			sendMessageParseHtml($ChatID, $message_body);
-			break;
-			
-		case "test": // This is the same text inside a Keyboard
-			$photoUrl ="http://ooxygen.tech/Glenda_Bot/Logo.png"; 
-			$photoDesc ="<b>Eccoti i Nostri Links</b> \n<a href='http://insane3dPrinting-forum.ooxygen.tech/'>Insane3dPrinting Forum</a> \n<a href='http://insane3d.ooxygen.tech/'>Insane3dPrinting Downloads</a>";
-			sendMessageImageAndLinks($ChatID, $photoUrl, $photoDesc);
-			break;
+    function sendMessage($chat_id, $text)
+    {
+        $url = $GLOBALS[website]./sendMessage?chat_id=.$chat_id.&text=.urlencode($text);
+        file_get_contents($url);
+    }
 
-		case 'Buongiorno':
-			$message_body = "<b>Buongiorno io Sono Glenda. \nPuoi Attivarmi Digitando il mio Nome.</b>";
-			sendMessageParseHtml($ChatID, $message_body);
-			break;
+    function showKeyboard($chat_id, $text)
+    {
+        $jSonCodeKeyboard = '&reply_markup={keyboard:[[%20Conigli%20]],resize_keyboard:true,one_time_keyboard:false}';
+        $url = $GLOBALS[website]./sendMessage?chat_id=.$chat_id.&text=.urlencode($text).$jSonCodeKeyboard;
+        file_get_contents($url);
+    }
 
-		case "Inline Keyboard": // This is the same text inside a Keyboard
-			$msg = "Abracadabra and inline keyboard will appear!";
-			inlineKeyboard($ChatID, $msg);
-			break;
-
-		default:
-			//$msg = "Unknown Command!";
-			//sendMessage($ChatID, $msg);
-			break;
-	} 
-	
-if(isset($message['voice']))
-{
-			$message_body = "<b>Non posso sentire messaggi audio purtroppo</b>";
-			sendMessageParseHtml($ChatID, $message_body);
-}
-
-	function sendMessage($chat_id, $text)
-	{
-		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&text=".urlencode($text);
-		file_get_contents($url);
-	}
-
-	function sendMessageParseHtml($chat_id, $text)
-	{
-		$url = $GLOBALS[website]."/sendMessage?chat_id=$chat_id&parse_mode=HTML&text=".urlencode($text);
-		file_get_contents($url);
-	}
-
-	function sendMessageParseHtmlTest($chat_id, $text)
-	{
-		$url = $GLOBALS[website]."/sendMessage?chat_id=$chat_id&parse_mode=HTML&text=".urlencode($text);
-		file_get_contents($url);
-	}
-
-	function sendMessageImageAndLinks($chat_id, $photoUrl, $photoDesc) // This is an useless type of this keyboard, in a specific Tutorial I show an useful usage of this keyboard.
-	{
-	    $url = $GLOBALS[website]."/sendPhoto?chat_id=".$chat_id."&photo=".$photoUrl."&parse_mode=HTML&caption=".urlencode($photoDesc);
-	    file_get_contents($url);
-	}
-
-	function showKeyboard($chat_id, $text)
-	{
-		$jSonCodeKeyboard = '&reply_markup={"keyboard":[["Links", "Tools"],["Infos","Help"]],"resize_keyboard":true}';
-		$url = $GLOBALS[website]."/sendMessage?chat_id=".$chat_id."&parse_mode=HTML&text=".urlencode($text).$jSonCodeKeyboard;
-		file_get_contents($url);
-	}
-	
-	function sendMessageImage($chat_id, $photoUrl, $photoDesc) // This is an useless type of this keyboard, in a specific Tutorial I show an useful usage of this keyboard.
-	{
-	    $url = $GLOBALS[website]."/sendPhoto?chat_id=".$chat_id."&photo=".$photoUrl."&caption=".urlencode($photoDesc);
-	    file_get_contents($url);
-	}
-	
-	function sendStartImage($chat_id, $photoUrl, $photoDesc) // This is an useless type of this keyboard, in a specific Tutorial I show an useful usage of this keyboard.
-	{
-		$jSonCodeKeyboard = '&reply_markup={"keyboard":[["Links", "Tools"],["Infos","Help"]],"resize_keyboard":true}';
-	    $url = $GLOBALS[website]."/sendPhoto?chat_id=".$chat_id."&photo=".$photoUrl."&caption=".urlencode($photoDesc).$jSonCodeKeyboard;
-	    file_get_contents($url);
-	}
+?>
