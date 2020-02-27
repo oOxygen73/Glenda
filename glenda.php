3624aa13d74845a53e74f3d5373625ba4b488fce
oOxygen73 <45863533+oOxygen73@users.noreply.github.com>
2020-02-27 03:28:24 +0100

    Update glenda.php

--- glenda.php
+++ glenda.php
@@ -1,17 +1,31 @@
 <?php
-$content = file_get_contents("php://input");
+header('Content-Type: application/json');
+$content = file_get_contents('php://input');
 $update = json_decode($content, true);
 
-if(! $update) {
-	exit;
+if (!$update) {
+    exit;
 }
 
 $chatId = $update['message']['chat']['id'] ?? null;
-$text = $update['message']['text'] ?? null;
-$FirstName = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-if(! $chatId || ! $text) {
-	exit;
-}
+$text = $update['message']['text'] ?? 'Inserire un testo valido';
 
-header("Content-Type: application/json");
-echo json_encode(['chat_id' => $chatId, 'first_name' => $FirstName, 'text' => $text, 'method' => 'sendMessage']);
+$parameters = [];
+
+$parameters['chat_id'] = $chatId;
+$parameters['text'] = $text;
+$parameters['method'] = 'sendMessage';
+
+$parameters['reply_markup'] = [
+    'keyboard' => [
+        ['Spaghetti Cacio e pepe', 'Spaghetti alla Carbonara'],
+        ['Pappardelle al Ragù', 'Pappardelle ai Funghi'],
+        ['Strozzapreti al Pesto', 'Fusilli al pomodoro'],
+        ['Test 1', 'Test 2', 'Test 3'],
+        ['Nuova comanda [010]', 'Precedente comanda [009]']
+    ],
+    'one_time_keyboard' => true,
+    'resize_keyboard' => true
+];
+
+echo json_encode($parameters);
